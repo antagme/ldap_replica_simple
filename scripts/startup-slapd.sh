fae811c57db750dbb9261f8870ab14767b5b26a7
Pedro Romero Aguado <ff7maniac@gmail.com>
2017-03-29 18:50:22 +0200

    Update startup-slapd.sh

--- scripts/startup-slapd.sh
+++ scripts/startup-slapd.sh
@@ -4,8 +4,6 @@
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 rm -rf /etc/openldap/slapd.d/*
 slaptest -f /opt/docker/slapd-edt.org.acl.conf -F /etc/openldap/slapd.d
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/organitzacio_edt.org.ldif
-slapadd -F /etc/openldap/slapd.d -l /opt/docker/usuaris-edt.org.ldiff
 chown -R ldap.ldap /etc/openldap/slapd.d/
 chown -R ldap.ldap /var/lib/ldap/
 /usr/sbin/slapd
