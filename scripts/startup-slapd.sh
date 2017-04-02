3c74516e2e2c336f18057d221517e081fbefabbd
Pedro Romero Aguado <ff7maniac@gmail.com>
2017-04-02 17:23:31 +0200

    Update startup-slapd.sh

--- scripts/startup-slapd.sh
+++ scripts/startup-slapd.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 
 /usr/bin/echo "Creating DB edt.org..."
+/usr/bin/echo "admin" | kinit admin/admin
 cp /usr/share/openldap-servers/DB_CONFIG.example /var/lib/ldap/DB_CONFIG
 rm -rf /etc/openldap/slapd.d/*
 slaptest -f /opt/docker/slapd-edt.org.acl.conf -F /etc/openldap/slapd.d
 chown -R ldap.ldap /etc/openldap/slapd.d/
 chown -R ldap.ldap /var/lib/ldap/
 /usr/sbin/slapd
-
+/usr/bin/echo "Contacting to master LDAP, Wait please..." && sleep 15
+kdestroy
+/usr/bin/echo "Thanks for Waiting , Done"
